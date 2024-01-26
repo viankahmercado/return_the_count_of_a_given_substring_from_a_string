54e088cde297f6bd70a35f9db82452b42a2a952a
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 20:47:53 +0800

    pseudocode

--- exercise_7.py
+++ exercise_7.py
@@ -5,3 +5,7 @@
 # Exercise 7:
 # Return the count of a given substring from a string
 # Write a program to find how many times substring “Emma” appears in the given string.
+
+# assign variable
+
+# create logic using count
