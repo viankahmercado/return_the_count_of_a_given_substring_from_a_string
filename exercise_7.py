410357c694b73986d28f9a1795d65183bc9d999b
viankahmercado <angelviankahmercado@gmail.com>
2024-01-26 20:38:47 +0800

    description

---
+++ exercise_7.py
@@ -0,0 +1,7 @@
+# Assignment 5:
+# Do the exercise 1-15 in https://pynative.com/python-basic-exercise-for-beginners/
+# Try do challenge yourself by not checking the "solution"
+
+# Exercise 7:
+# Return the count of a given substring from a string
+# Write a program to find how many times substring “Emma” appears in the given string.
